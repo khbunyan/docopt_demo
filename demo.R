308a35acb53cf4071e2971f5ea981f3ccd569cb7
khbunyan <69618055+khbunyan@users.noreply.github.com>
2021-11-18 14:22:16 -0800

    This commit will add the argument and print statement

--- demo.R
+++ demo.R
@@ -8,14 +8,11 @@ Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-[<arg4>]   Takes any value (this is an optional positional argument)
+[<arg4>]          Takes any value (this is an optional positional argument)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
-
-main <- function (arg1, arg2, arg3=NULL, arg4=NULL) {
-    print(opt)
-    print(typeof(opt))
-}
-main()
+print(opt)
+print(typeof(opt))
+print(opt$arg4)
